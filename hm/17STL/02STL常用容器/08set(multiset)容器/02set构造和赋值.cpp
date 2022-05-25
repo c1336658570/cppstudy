ec53ddbe9b94d61a7a5978e936f24ab330cc5b90
c1336658570 <1336658570@qq.com>
2022-05-25 10:11:18 +0800

    c++ study

---
+++ hm/17STL/02STL常用容器/08set(multiset)容器/02set构造和赋值.cpp
@@ -0,0 +1,63 @@
+/*
+set构造和赋值
+
+功能描述：创建set容器以及赋值
+
+构造：
+set<T> st;                        //默认构造函数：
+set(const set &st);       //拷贝构造函数
+
+赋值：
+set& operator=(const set &st);    //重载等号操作符
+
+总结：
+set容器插入数据时用insert
+set容器插入数据的数据会自动排序
+*/
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+void printSet(set<int> &s)
+{
+    for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+//构造和赋值
+void test01()
+{
+    set<int> s1;
+
+    //插入数据 只有insert方式
+    s1.insert(10);
+    s1.insert(30);
+    s1.insert(20);
+    s1.insert(40);
+    s1.insert(30);
+
+    //便利容器
+    // set容器特点：所有元素插入时被自动排序
+    // set容器不允许插入重复值
+    printSet(s1);
+
+    //拷贝构造
+    set<int> s2(s1);
+    printSet(s2);
+
+    //赋值
+    set<int> s3;
+    s3 = s2;
+    printSet(s3);
+}
+
+int main()
+{
+    test01();
+
+    return 0;
+}
