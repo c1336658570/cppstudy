af0efb8d1ef863d949670876996529c3bf3d5dfa
c1336658570 <1336658570@qq.com>
2022-05-27 22:53:51 +0800

    c++ study

---
+++ hm/18演讲比赛流程管理系统/speechManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//演讲管理类
+class SpeechManager
+{
+public:
+    //构造函数
+    SpeechManager();
+
+    //展示菜单
+    void show_Menu();
+
+    //退出系统
+    void exitSystem();
+
+    //析构函数
+    ~SpeechManager();
+};
--- hm/18演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
+++ hm/18演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -1,3 +1,39 @@
 /*
     演讲比赛流程管理系统
 */
+
+#include <iostream>
+using namespace std;
+#include "speechManager.h"
+
+int main()
+{
+    SpeechManager sm;
+
+    int choice = 10; //用来存储用户的选项
+
+    while (true)
+    {
+        sm.show_Menu();
+
+        cout << "请输入您的选择： " << endl;
+        cin >> choice; // 接受用户的选项
+
+        switch (choice)
+        {
+        case 1: //开始比赛
+            break;
+        case 2: //查看记录
+            break;
+        case 3: //清空记录
+            break;
+        case 0: //退出系统
+            sm.exitSystem();
+            break;
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
